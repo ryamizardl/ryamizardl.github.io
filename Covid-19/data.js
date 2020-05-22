ae6daea2c5e496f9d91974d787d0793f0733be7e
ryamizardl <53049567+ryamizardl@users.noreply.github.com>
2020-05-22 22:17:41 +0700

    Delete data.js

--- Covid-19/data.js
+++
@@ -1,9 +0,0 @@
-var f = document.createElement("iframe");
-  f.src = "https://widget.kopi.dev/corona";
-  f.width = "100%";
-  f.height = 380;
-  f.scrolling = "no";
-  f.frameBorder = 0;
-  var rootEl = document.getElementById("kopi-covid");
-  console.log(rootEl);
-  rootEl.appendChild(f);
